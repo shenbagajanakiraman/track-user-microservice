4eb8b30c94cb6909270db07a9425821671ab8a36
shenbaga Lakshmi J <shenbaga30may@gmail.com>
2019-05-28 15:01:28 +0530

    Added track-service

---
+++ track-service/src/main/java/com/stackroute/domain/Track.java
@@ -0,0 +1,22 @@
+package com.stackroute.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class Track
+{
+
+    @Id
+    private int trackId;
+    private String trackName;
+    private String comments;
+
+}
