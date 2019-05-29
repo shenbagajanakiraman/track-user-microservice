fb3e1bc11ca150a6e06d82c130bba8512a2e88e5
Gargi <anngargi@gmail.com>
2019-05-29 16:03:33 +0530

    config server modified

--- config-server/src/main/java/com/stackroute/configserver/SpringConfigServerApplication.java
+++ config-server/src/main/java/com/stackroute/configserver/SpringConfigServerApplication.java
@@ -8,9 +8,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class SpringConfigServerApplication {
 
-	public static void main(String[] args)
-	{
-		SpringApplication.run(SpringConfigServerApplication.class, args);
-	}
+    public static void main(String[] args)
+
+    {
+        SpringApplication.run(SpringConfigServerApplication.class, args);
+    }
 
 }
